24e6beb8c63e127ec1d2219e0dafb4d2a096fd5d
Ariella Tigertail <ariellatg@gmail.com>
2018-02-01 12:53:00 -0500

    second

--- hw/p5/Getting Started/sketch.js
+++ hw/p5/Getting Started/sketch.js
@@ -1,5 +1,19 @@
 var centerX = 100;
 var centerY = 200;
+var number = 1;
+var number = 2;
+var letter1 = 'a';
+var letter2 = 'b';
+var greeting = "hello";
+var greeting2 = "goodbye";
+var truthordare = true;
+var truthordare2 = false;
+
+
+
+
+
+
 
 
 function setup() {
---
+++ hw/p5/Getting Started/sketch2.js
@@ -0,0 +1,21 @@
+var centerX = 100;
+var centerY = 200;
+var number = 1;
+var number = 2;
+var letter1 = 'a';
+var letter2 = 'b';
+var greeting = "hello";
+var greeting2 = "goodbye";
+var truthordare = true;
+var truthordare2 = false;
+
+
+
+function setup() {
+  // put setup code here
+
+}
+
+function draw() {
+  // put drawing code here
+}
