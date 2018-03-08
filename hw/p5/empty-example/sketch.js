9b38934506db80f45ca190a229a513d630e95dcc
Ariella Tigertail <ariellatg@gmail.com>
2018-03-07 21:40:16 -0500

    chromaticity

--- hw/p5/empty-example/sketch.js
+++ hw/p5/empty-example/sketch.js
@@ -6,11 +6,17 @@ var greeting2 = "goodbye";
 
 
 
-
-
-
 function setup() {
   // put setup code here
+  console.log("Hello World!");
+  console.log(number1);
+  console.log("The value of letter2" + letter2);
+
+  console.log(number1 + number2);
+  console.log(number2 + letter2);
+
+//does not work
+  console.log(number2)
 
 }
 
