b4d3d6c716479f9cee9f190e2c098a053ecb2b22
Ariella Tigertail <ariellatg@gmail.com>
2018-05-07 16:34:07 -0400

    done

---
+++ hw/Assignment 5/sketch.js
@@ -0,0 +1,48 @@
+var fft, noise, filter;
+
+function setup() {
+  createCanvas(200,200);
+  fill('#fae');
+
+
+  filter = new p5.BandPass();
+
+  noise = new p5.Noise();
+  // disconnect unfiltered noise,
+  // and connect to filter
+  noise.disconnect();
+  noise.connect(filter);
+  noise.start();
+
+  fft = new p5.FFT();
+}
+
+function draw() {
+  background(30);
+
+  // set the BandPass frequency based on mouseX
+  var freq = map(mouseX, 0, width, 20, 10000);
+  filter.freq(freq);
+  // give the filter a narrow band (lower res = wider bandpass)
+  filter.res(100);
+
+  // draw filtered spectrum
+  var spectrum = fft.analyze();
+  noStroke();
+  for (var i = 0; i < spectrum.length; i++) {
+    var x = map(i, 0, spectrum.length, 0, width);
+    var h = -height + map(spectrum[i], 0, 255, height, 0);
+    rect(x, height, width/spectrum.length, h);
+  }
+
+  isMouseOverCanvas();
+}
+
+function isMouseOverCanvas() {
+  var mX = mouseX, mY = mouseY;
+  if (mX > 0 && mX < width && mY < height && mY > 0) {
+    noise.amp(0.8, 0.2);
+  } else {
+    noise.amp(0, 0.8);
+  }
+}
